c1a0a21e3beff94921477351e6e31ff38df1f561
Mikhail_Svetlov <jegnum@gmail.com>
2024-10-20 22:46:37 +0400

    Added a test shopping option

--- source/bot.ts
+++ source/bot.ts
@@ -1,4 +1,4 @@
-import { Bot, InlineKeyboard } from "@grammyjs/bot";
+import { Bot, InlineKeyboard, } from "@grammyjs/bot";
 
 
 const bot = new Bot("7593247270:AAF296ILhtKs3fBafeHqR_1XLOLz5Akx7PU"); // <-- put your bot token between the ""
@@ -22,8 +22,24 @@ bot.callbackQuery("parcel", async (ctx) => {
     await ctx.answerCallbackQuery(); // Закрываем всплывающее окно
     await ctx.reply("Вам посылка!");  // Отправляем сообщение
   });
-  
-bot.command("test", (ctx) => ctx.replyWithSticker("CAACAgIAAxkBAAMLZxUZga2vQ_6WRs2XWwb_hEL773AAAvkHAAJFUZMOYcv-eWLVIEY2BA"));
+
+
+
+  bot.command("test", async (ctx) => {
+    await ctx.replyWithInvoice(
+      "Покупка нихуя", // title
+      "Купите нихуя за одну звезду!", // description
+      "book_purchase_payload", // payload
+      "XTR", // currency
+      [{ label: "Nothing", amount: 1 }],
+    );
+  });
+
+bot.callbackQuery("book_purchase_payload", async (ctx) => {
+    await ctx.answerPreCheckoutQuery(true);
+    await ctx.answerCallbackQuery();
+    await ctx.reply("Спасибо за покупку!");
+  });
 
   bot.on("message:text", async (ctx) => {
     const message = ctx.message.text;
@@ -43,8 +59,5 @@ bot.command("test", (ctx) => ctx.replyWithSticker("CAACAgIAAxkBAAMLZxUZga2vQ_6WR
   });
 
 
-// Now that you specified how to handle messages, you can start your bot.
-// This will connect to the Telegram servers and wait for messages.
 
-// Start the bot.
 bot.start();
